88bcf342f4e15785d7505c8ce6f2fcc16bb69c97
srvsrr <rohannandan2004@gmail.com>
2024-03-12 15:06:02 +1200

    cs111

---
+++ cs111.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+cout << "\n\tGrade List for CS111" << endl
+     << "\t--------------------\n\n";
+cout << "\tGoogle Smith\t\tA" << endl; 
+cout << "\tJohn Pencil\t\tB" << endl
+<< "\tMr. Football\t\tC" << endl; 
+cout << "\tJason Stone\t\tB+"
+<< endl << "\tStapler Pin\t\tA+" << endl;
+cout << "\tManila Folder\t\tC+" << endl;
+cout <<
+ "\tKeyboard Hunter\t\tD" << endl; 
+cout << "\tSystem Speaker\t\tE" << endl;
+cout << endl << "\n";
+system("PAUSE");
+return 0;
+}
