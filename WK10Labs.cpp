c76f2b1dc1447f9b860d62cea471c6bdaff954c4
Rohan Nandan <rohannandan2004@gmail.com>
2024-05-07 16:42:53 +1200

    Update WK10Labs.cpp

--- WK10Labs.cpp
+++ WK10Labs.cpp
@@ -45,7 +45,7 @@ void random_pair(int &first, int &second)
     second = rand() % (MAXINT - MININT + 1) + MININT;
 }
 
-bool check_linear(int guess, int answer)
+bool check_linear(int guess, int answer, int &score)
 {
     bool is_equal;
 
@@ -53,6 +53,8 @@ bool check_linear(int guess, int answer)
     {
         cout << "Great!\n\n";
         is_equal = true;
+        score++;
+        cout << "Your score is " << score << endl;
     }
 
     else
@@ -64,14 +66,16 @@ bool check_linear(int guess, int answer)
     return is_equal;
 }
 
-bool check_quadratic(int guess, int answer1, int answer2)
+bool check_quadratic(int &guess1, int &guess2, int answer1, int answer2, int &score)
 {
     bool is_equal;
 
-    if(guess == answer1 || guess == answer2)
+    if(guess1 == answer1 && guess2 == answer2 || guess2 == answer1 && guess1 == answer2)
     {
         cout << "Great!\n\n";
         is_equal = true;
+        score++;
+        cout << "Your score is " << score << endl;
     }
 
     else
@@ -83,14 +87,11 @@ bool check_quadratic(int guess, int answer1, int answer2)
     return is_equal;   
 }
 
-
-
-
 int main()
 {
     int a = 0;
     int b = 0;
-    int x;
+    int x, y;
     int score = 0;
     int tries = 0;
     char reply;
@@ -115,10 +116,7 @@ int main()
             cout << coeff_1 << " x " << signchar(coeff_2) << " " << fabs(coeff_2) << " = 0\n";
             x = readint();
 
-            if(check_linear(x, solution))
-            {
-                score++;
-            }
+            if(check_linear(x, solution, score));
         }
 
         else
@@ -129,12 +127,11 @@ int main()
             int coeff_2  = a * b;
             cout << "\nWhat is a solution to\n";
             cout << "x^2 " << signchar(coeff_1) << " " << fabs(coeff_1) << " x "  << signchar(coeff_2) << " " << fabs(coeff_2) << " = 0\n"; //Use function to fix formatting.
-            x = readint();// Use function to validate
+            x = readint();
+            y = readint();// Use function to validate
 
-            if(check_quadratic(x, solution1, solution2))
-            {
-                score++;
-            }
+            if(check_quadratic(x,y, solution1, solution2, score));
+            tries++;
         }
 
         cout << "Enter 'L'' to try a linear  and 'Q' to try a quadratic equation.\nAny other key to quit.";
