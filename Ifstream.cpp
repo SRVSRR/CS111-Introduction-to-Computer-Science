1c16c35b3f0b105f7046230d347bc51774153725
srvsrr <rohannandan2004@gmail.com>
2024-05-20 08:26:00 +1200

    Fix: File O/I

--- Ifstream.cpp
+++ Ifstream.cpp
@@ -6,7 +6,8 @@ using namespace std;
 int main(){
     ifstream input;
     input.open("num.txt");
-    ofstream output("sum.txt");
+    ofstream output;
+    output.open("sum.txt");
 
     string header;
     getline(input, header); // read and ignore the header line
@@ -17,19 +18,20 @@ int main(){
     string header2;
     getline(input, header2); 
 
-    int num1, num2;
-    int sum = 0;
-    output << header << endl;
+    output << header << endl; 
     output << header1 << endl;
     output << header2 << endl;
 
+    int num1, num2;
+    int sum = 0;    
+    
     while (input >> num1 >> num2){
         sum += num1;
         output << num1 << " " << num2 << endl;
     }
 
     input.close();
-    
+
     // Write the sum
     output << "\nThe sum of the first column is: " << sum << endl;
 
