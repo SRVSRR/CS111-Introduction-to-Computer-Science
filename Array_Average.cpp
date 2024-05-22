adbd3968be2c8c0e199f980a9d4507377a80cb6e
Rohan Nandan <rohannandan2004@gmail.com>
2024-05-22 12:49:17 +1200

    Feat: array average with setprecision

---
+++ Array_Average.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+double avg(int value[], int SIZE){
+    double sum = 0;
+    for(int i = 0; i < SIZE; i++){
+        sum+=value[i];
+    }
+    double average = sum/SIZE;
+    return average;
+}
+
+int main(){
+    cout << fixed << setprecision(1);
+    const int SIZE = 5;
+    int values[SIZE] = {1, 2, 3, 4, 5};
+
+    double average = avg(values, SIZE);
+
+    cout << average << endl;    
+    return 0;
+}
