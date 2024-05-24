3e2928253086b9a205dbcad060febb05ce607004
Rohan Nandan <rohannandan2004@gmail.com>
2024-05-25 00:39:01 +1200

    Feat: 2D Array Diagonal Sums

---
+++ Diagonal_Array.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    const int row = 5;
+    const int columns = 5;
+    int values[row][columns] = 
+    {
+     2,  4,  6,  4, 18,
+    43, 23, 67, 78, 32,
+    13, 10, 11, 21, 21, 
+    45, 44, 53, 65, 76, 
+    34, 43, 43, 99, 84
+    };
+
+    // Diagonal Sum
+    int diagonal_sum = 0;
+    for(int i = 0;i < row; i++)
+    {
+        cout << values[i][i] << " ";
+        diagonal_sum += values[i][i];
+    }
+    cout << "Diagonal Sum = " << diagonal_sum << endl;
+
+    // Anti Diagonal Sum
+    int anti_diagonal_sum = 0;
+    int j = 4;
+    for(int i = 0; i < row; i++)
+    {
+        cout << values[j][i] << " ";
+        anti_diagonal_sum += values[j][i];
+        j--;
+    }
+    cout << "Anti-Diagonal Sum = " << anti_diagonal_sum << endl;
+    return 0;
+}
