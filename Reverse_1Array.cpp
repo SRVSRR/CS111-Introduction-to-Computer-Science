b587176ab5028efd453f64d0c3dfcce0c8619bf4
Rohan Nandan <rohannandan2004@gmail.com>
2024-05-24 23:56:13 +1200

    Feat: ST Question

---
+++ Reverse_1Array.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+void reverse(int values[], int CAP){
+    int y = 5;
+    for(int i = 0; i < CAP; i++){
+        int temp = values[i];
+        values[i] = values[y];
+        values[y] = temp;
+        y--;
+    }
+}
+
+int main(){
+    const int CAP = 3;
+    int values[] = {1, 2, 3, 4, 5, 6}; 
+    
+    reverse(values, CAP);
+
+    for(int i = 0; i < CAP; i++){
+        cout << values[i];
+    }
+
+    return 0;
+}
